0ee017e77df8292c6ce8376527fb4c29df84abd4
Jeffrey Wilcke <stygeo@gmail.com>
2013-05-08 15:52:31 +0200

    added tests

---
+++ tests/app.php
@@ -0,0 +1,22 @@
+<?
+include("../kojn_php/kojn.php");
+
+Kojn::log("This app serves as a php Kojn PHP test.");
+
+$test_object = new StdClass();
+$test_object->hello = 'world';
+Kojn::log($test_object);
+
+$kojn = Kojn::setup(function($config) {
+  Kojn::log("in #setup - Setting up kojn");
+  $config::$api_key = "09367d4b49ae1f1d9ee0a326bd3619f1";
+
+  $config::$host    = "localhost";
+  $config::$port    = 3000;
+  $config::$ssl     = false;
+});
+
+Kojn::log("Fetching all transactions");
+var_dump(Kojn_list_invoices($kojn));
+
+?>
